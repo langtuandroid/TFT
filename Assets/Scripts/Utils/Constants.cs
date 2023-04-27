f796fadf9d0f6e103b29e27026bb50e0bb6a7dc3
David Díaz Roldán <davih22997@gmail.com>
2023-04-28 01:08:19 +0200

    Añadido script de antorcha y clase para las constantes.

---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,15 @@
+namespace Utils
+{
+    public class Constants
+    {
+        #region Tags
+        #region Fire Attacks
+        public const string TAG_FIRE_BALL = "FireBall";
+        public const string TAG_FLAMES = "Flames";
+        #endregion
+        #endregion
+    }
+}
+
+
+
