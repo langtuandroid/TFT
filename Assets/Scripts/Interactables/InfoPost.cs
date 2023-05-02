fe6439362952cd3d4fbca3ee22b544791e212af1
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-05-02 14:02:06 +0200

    Interaction, Chest & InfoPost Script/Prefab Fixed

--- Assets/Scripts/Interactables/InfoPost.cs
+++ Assets/Scripts/Interactables/InfoPost.cs
@@ -2,40 +2,13 @@ using UnityEngine;
 
 public class InfoPost : MonoBehaviour, IInteractable
 {
-    [SerializeField] private string InfoPostMessage;
-
-    [SerializeField] private bool canRead = false;
+    [SerializeField] private string _infoPostMessage;
 
     public void Interact( Vector2 lookDirection )
     {
-        Debug.Log( lookDirection );
-        if ( canRead && lookDirection.y > 1 )
-            Debug.Log( InfoPostMessage );
+        if ( lookDirection.y > 0 )
+            Debug.Log( _infoPostMessage );
         else
             Debug.Log( "Cannot Read From Here" );
-    }
-
-    private void OnCollisionEnter2D( Collision2D collision )
-    {
-        if ( collision.transform.CompareTag( "Player" ) )
-            canRead = true;
-    }
-
-    private void OnCollisionExit2D( Collision2D collision )
-    {
-        if ( collision.transform.CompareTag( "Player" ) )
-            canRead = false;
-    }
-
-    private void OnTriggerEnter2D( Collider2D collision )
-    {
-        if ( collision.CompareTag( "Player" ) )
-            canRead = true;
-    }
-
-    private void OnTriggerExit2D( Collider2D collision )
-    {
-        if ( collision.CompareTag( "Player" ) )
-            canRead = false;
     }
 }
--- Assets/Scripts/Player/Interaction.cs
+++ Assets/Scripts/Player/Interaction.cs
@@ -4,20 +4,33 @@ public class Interaction : MonoBehaviour
 {
     [Header("Interactable")]
     [SerializeField] private LayerMask _interactableLayer;
-    [SerializeField] private float _checkDistance = 1.0f;
+    [SerializeField] private float _checkDistance = 0.6f;
+    [SerializeField] private Vector2 _rayCastOffset = new( 0.2f , 0.2f );
 
     public void Interact( Vector2 colliderOffset , Vector2 lookDirection )
     {
-        Vector2 origin = new Vector2( colliderOffset.x + transform.position.x , 
-                                      colliderOffset.y + transform.position.y );
+        float xRayOffset = lookDirection.y != 0 ? _rayCastOffset.x : 0;
+        float yRayOffset = lookDirection.x != 0 ? _rayCastOffset.y : 0;
 
-        RaycastHit2D hit = Physics2D.Raycast( origin ,
-                lookDirection ,
-                _checkDistance , _interactableLayer );
+
+        Vector2 origin = new Vector2( colliderOffset.x + transform.position.x + xRayOffset, 
+                                      colliderOffset.y + transform.position.y + yRayOffset );
+
+        RaycastHit2D hit = Physics2D.Raycast( origin , lookDirection , _checkDistance , _interactableLayer );
 
         if ( hit )
         {
             hit.collider.GetComponent<IInteractable>()?.Interact( lookDirection );
+            return;
         }
+
+
+        origin = new Vector2( colliderOffset.x + transform.position.x - xRayOffset,
+                              colliderOffset.y + transform.position.y - yRayOffset );
+
+        hit = Physics2D.Raycast( origin , lookDirection , _checkDistance , _interactableLayer );
+
+        if ( hit )
+            hit.collider.GetComponent<IInteractable>()?.Interact( lookDirection );
     }
 }
