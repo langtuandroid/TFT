8d045d20f3bfb5c6683778e661b64b0f5fb24119
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-05-22 11:08:16 +0200

    Woods save zones ref and save data created

--- Assets/Scripts/Level Control/TriggerChangeZone.cs
+++ Assets/Scripts/Level Control/TriggerChangeZone.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 public class TriggerChangeZone : MonoBehaviour
 {
-    [SerializeField][Range( 0, 10 )] private int _nextStartPointRefID;
+    [SerializeField][Range( 0, 15 )] private int _nextStartPointRefID;
     [SerializeField] private ZoneExitSideSO _zoneExitSO;
 
     private void OnTriggerEnter2D( Collider2D collision )
