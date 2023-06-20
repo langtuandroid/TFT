42661b75c6928e0e90df727245270720b9cb327b
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-06-20 16:34:56 +0200

    SealMovement finally moves

--- Assets/Scripts/Altar/SealMovement.cs
+++ Assets/Scripts/Altar/SealMovement.cs
@@ -4,10 +4,14 @@ using DG.Tweening;
 public class SealMovement : MonoBehaviour
 {
     [SerializeField] private float _duration = 3f;
+    [SerializeField] private int _pixelToMove = 3;
 
-    private void Awake()
+    private void Start()
     {
         float pixel = 1f / 16;
-        transform.DOMoveY( pixel * _duration , 1 ).SetLoops( -1, LoopType.Yoyo ).Play();
+        transform.DOMoveY( pixel * _pixelToMove , _duration )
+            .SetLoops( -1 , LoopType.Yoyo )
+            .SetRelative()
+            .Play();
     }
 }
