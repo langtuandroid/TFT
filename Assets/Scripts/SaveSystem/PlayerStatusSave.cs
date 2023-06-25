fb55b4070af5fa18fa5b4125c06bbe1959bfd72c
davih22997 <davih22997@gmail.com>
2023-06-25 20:59:03 +0200

    Añadida navegación por menú inventario. -> Se muestran textos descriptivos. -> Las imágenes están contenidas en el Canvas. -> Añadida más información en PlayerStatus.

--- Assets/Scripts/SaveSystem/PlayerStatusSave.cs
+++ Assets/Scripts/SaveSystem/PlayerStatusSave.cs
@@ -13,23 +13,34 @@ public class PlayerStatusSave
     public int maxSouls;
 
     // Physics Skills
+    public bool isPhysicAttackUnlocked; // Si tiene el cetro
     public bool isJumpUnlocked;
     public bool isSwimUnlocked;
     public bool isDashUnlocked;
 
     // Primary skill
+    // Fire power
     public bool isFireWeakUnlocked;
     public bool isFireMediumUnlocked;
     public bool isFireStrongUnlocked;
     
+    // Plant power
     public bool isPlantWeakUnlocked;
     public bool isPlantMediumUnlocked;
     public bool isPlantStrongUnlocked;
     
+    // Water power
     public bool isWaterWeakUnlocked;
     public bool isWaterMediumUnlocked;
     public bool isWaterStrongUnlocked;
 
     // Secondary skill
+    // Light power
     public bool isLightUnlocked;
+    // Air power
+    public bool isAirUnlocked;
+
+    // Objects
+    public bool lifeBerryUnlocked;
+    public int lifeBerryQuantity;
 }
--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -11,39 +11,61 @@ public class Inventory : MonoBehaviour
 {
     #region SerializeFields
 
-    [SerializeField]
-    [Tooltip("Información del objeto seleccionado")]
-    private TMP_Text _textInfo;
+    [Header("Game information")]
 
     [SerializeField]
-    [Tooltip("Objecto \"vacío\"")]
-    private Item _empty;
+    [Tooltip("Información del juego")]
+    private PlayerStatusSaveSO _playerStatusSO;
+
+    [SerializeField]
+    [Tooltip("Lista con las descripciones de los objetos")]
+    private List<string> _descriptions;
+
+    [Header("UI elements")]
+
+    [SerializeField]
+    [Tooltip("Lista de botones de objetos no equipables")]
+    private List<Button> _nonEquipableButtons;
+
+    [SerializeField]
+    [Tooltip("Parte que muestra la descripción del objeto seleccionado")]
+    private TMP_Text _textInfo;
+
+    [Header("Input Action Asset")]
+
+    [SerializeField]
+    [Tooltip("Input action asset")]
+    private InputActionAsset _inputActions;
 
     #endregion
 
 
     #region Public variables
 
-    public Sprite exampleSprite;
-    public List<Item> items = new List<Item>();
-    public List<Item> nonEquipableList;
-    public List<Image> nonEquipableButtons;
-    public List<Item> powerList;
-    public List<ConsumableItem> consumableList;
-    public int inventorySpace = 10;
+    //public List<Item> items = new List<Item>();
+    //public List<Item> nonEquipableList;
+
+    //public List<Item> powerList;
+    //public List<ConsumableItem> consumableList;
 
     #endregion
 
     #region Private variables
-    private Dictionary<int, Item> uiItem = new Dictionary<int, Item>();
-    private Dictionary<int, GameObject> uiItems = new Dictionary<int, GameObject>();
-    private readonly Dictionary<int, bool> itemUIStates = new Dictionary<int, bool>();
 
-    private List<int> itemsNoEquipables = new List<int>();
+    // SERVICES
+    //private GameInputs _gameInputs;
+
+    // ELEMENTS
+    private Button _currentSelected;
+    //private Dictionary<int, Item> uiItem = new Dictionary<int, Item>();
+    //private Dictionary<int, GameObject> uiItems = new Dictionary<int, GameObject>();
+    //private readonly Dictionary<int, bool> itemUIStates = new Dictionary<int, bool>();
+
+    //private List<int> itemsNoEquipables = new List<int>();
 
     // SERVICES
     private InputAction submitAction;
-    public InputActionAsset inputActions;
+
 
     #endregion
 
@@ -51,22 +73,28 @@ public class Inventory : MonoBehaviour
 
     private void Awake() //TODO quitar solo es de prueba
     {
-        submitAction = inputActions.FindActionMap("UI").FindAction("Navigate");
-        Item yellowFlower = new Item("Flor amarillenta", null, "Me encanta esta floresita que es de color amarilla");
-        yellowFlower.itemID = 1;
-        //Item florAmarillenta = new Item("Flor Amarillenta", 1);
-        AddItem(yellowFlower);
-    }
-
-    private void Start()
-    {
-        Init();
+        submitAction = _inputActions.FindActionMap("UI").FindAction("Navigate");
+        //Item yellowFlower = new Item("Flor amarillenta", null, "Me encanta esta floresita que es de color amarilla");
+        //yellowFlower.itemID = 1;
+        ////Item florAmarillenta = new Item("Flor Amarillenta", 1);
+        //AddItem(yellowFlower);
     }
 
     private void OnEnable()
     {
+        Init();
+
+        foreach (Button button in _nonEquipableButtons)
+            button.onClick.AddListener(() => Debug.Log(button.gameObject.name));
+
         submitAction.performed += OnMovement;
         submitAction.Enable();
+
+        _currentSelected = _nonEquipableButtons[0];
+        _currentSelected.Select();
+
+        if (_playerStatusSO.playerStatusSave.isPhysicAttackUnlocked)
+            SetText(_descriptions[0]);
     }
 
     private void OnDestroy()
@@ -81,21 +109,27 @@ public class Inventory : MonoBehaviour
 
     private void Init()
     {
-        foreach (var item in nonEquipableList)
-            uiItem.Add(item.itemID, item);
+        //_gameInputs = ServiceLocator.GetService<GameInputs>();
 
-        for (int i = 0; i < nonEquipableButtons.Count; i++)
-        {
-            if (i < nonEquipableList.Count)
-                nonEquipableButtons[i].sprite = nonEquipableList[i].Sprite;
-            else
-                nonEquipableButtons[i].sprite = _empty.Sprite;
-        }
+        // Mostramos los iconos
+        ShowIcons();
 
 
-        EventSystem ev = EventSystem.current;
-        ev.SetSelectedGameObject(nonEquipableButtons[0].transform.parent.gameObject);
-        _textInfo.text = nonEquipableList[0].Description;
+
+        //foreach (var item in nonEquipableList)
+        //    uiItem.Add(item.itemID, item);
+
+        //for (int i = 0; i < nonEquipableButtons.Count; i++)
+        //{
+        //    if (i < nonEquipableList.Count)
+        //        nonEquipableButtons[i].sprite = nonEquipableList[i].Sprite;
+        //    else
+        //        nonEquipableButtons[i].sprite = _empty.Sprite;
+        //}
+
+
+
+        //_textInfo.text = nonEquipableList[0].Description;
 
 
         //foreach (Item item in items) // Busqueda de items no equipables en la ui para añadirlas al diccionario
@@ -118,18 +152,36 @@ public class Inventory : MonoBehaviour
         //}
     }
 
-    private void CheckItemInInventory(int itemID)
+    private void ShowIcons()
     {
-        if (itemUIStates.ContainsKey(itemID))
-        {
-            bool isActive = itemUIStates[itemID];
+        // Vamos viendo si ha desbloqueado los distintos elementos
+        PlayerStatusSave ps = _playerStatusSO.playerStatusSave;
 
-            if (uiItems.ContainsKey(itemID))
-            {
-                uiItems[itemID].SetActive(isActive);
-            }
-        }
+        // Vara:
+        if (ps.isPhysicAttackUnlocked)
+            _nonEquipableButtons[0].GetComponentInChildren<Image>().enabled = true;
+
+        // Botas
+        if (ps.isJumpUnlocked)
+            _nonEquipableButtons[1].GetComponentInChildren<Image>().enabled = true;
+
+        // Dash
+        if (ps.isDashUnlocked)
+            _nonEquipableButtons[2].GetComponentInChildren<Image>().enabled = true;
     }
+
+    //private void CheckItemInInventory(int itemID)
+    //{
+    //    if (itemUIStates.ContainsKey(itemID))
+    //    {
+    //        bool isActive = itemUIStates[itemID];
+
+    //        if (uiItems.ContainsKey(itemID))
+    //        {
+    //            uiItems[itemID].SetActive(isActive);
+    //        }
+    //    }
+    //}
 
     #endregion
 
@@ -137,54 +189,90 @@ public class Inventory : MonoBehaviour
 
     public void OnMovement(InputAction.CallbackContext ctx)
     {
-        Debug.Log("Cambio selección");
-        EventSystem ev = EventSystem.current;
-        int n = int.Parse(ev.currentSelectedGameObject.name);
-        if (uiItem.ContainsKey(n))
+        Invoke(nameof(ChangeText), .1f);
+    }
+
+    private void ChangeText()
+    {
+        EventSystem current = EventSystem.current;
+
+        _currentSelected = current.currentSelectedGameObject.GetComponent<Button>();
+
+        PlayerStatusSave ps = _playerStatusSO.playerStatusSave;
+
+
+        int i = 0;
+
+        if (_nonEquipableButtons.Contains(_currentSelected))
+            for (i = 0; i < _nonEquipableButtons.Count && _currentSelected != _nonEquipableButtons[i]; i++) ;
+
+        switch (i)
         {
-            _textInfo.text = uiItem[n].Description;
-        }
-        else
-        {
-            _textInfo.text = _empty.Description;
+            case 0:
+                if (ps.isPhysicAttackUnlocked)
+                    SetText(_descriptions[0]);
+                else
+                    SetText("");
+                break;
+            case 1:
+                if (ps.isJumpUnlocked)
+                    SetText(_descriptions[1]);
+                else
+                    SetText("");
+                break;
+            case 2:
+                if (ps.isDashUnlocked)
+                    SetText(_descriptions[2]);
+                else
+                    SetText("");
+                break;
+            default:
+                SetText("");
+                break;
         }
     }
 
-    public bool AddItem(Item item)
+    private void SetText(string text)
     {
-        if (items.Count < inventorySpace)
-        {
-            items.Add(item);
-            itemUIStates[item.itemID] = true;
-            if (uiItems.ContainsKey(item.itemID))
-            {
-                uiItems[item.itemID].SetActive(true);
-            }
-            return true;
-        }
-        else
-        {
-            Debug.Log("Inventario lleno. No se puede añadir " + item.Name + ".");
-            return false;
-        }
+        _textInfo.text = text;
     }
 
-    public void RemoveItem(Item item)
-    {
-        if (items.Contains(item))
-        {
-            items.Remove(item);
-            itemUIStates.Remove(item.itemID);
-            if (uiItems.ContainsKey(item.itemID))
-            {
-                uiItems[item.itemID].SetActive(false);
-            }
-        }
-        else
-        {
-            Debug.Log("No se encontró " + item.Name + " en el inventario.");
-        }
-    }
+
+    //public bool AddItem(Item item)
+    //{
+    //    if (items.Count < inventorySpace)
+    //    {
+    //        items.Add(item);
+    //        itemUIStates[item.itemID] = true;
+    //        if (uiItems.ContainsKey(item.itemID))
+    //        {
+    //            uiItems[item.itemID].SetActive(true);
+    //        }
+    //        return true;
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("Inventario lleno. No se puede añadir " + item.Name + ".");
+    //        return false;
+    //    }
+    //}
+
+    //public void RemoveItem(Item item)
+    //{
+    //    if (items.Contains(item))
+    //    {
+    //        items.Remove(item);
+    //        itemUIStates.Remove(item.itemID);
+    //        if (uiItems.ContainsKey(item.itemID))
+    //        {
+    //            uiItems[item.itemID].SetActive(false);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Debug.Log("No se encontró " + item.Name + " en el inventario.");
+    //    }
+    //}
 
     #endregion
 
