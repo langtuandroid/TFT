da0d76b18a18be4c77d28f710a87bd94a9c3fdf2
davih22997 <davih22997@gmail.com>
2023-06-27 16:48:20 +0200

    Añadida cantidad de magia máxima a 30 para que no se quede aturdido Alex.

--- Assets/Scripts/SaveSystem/SO/PlayerStatusSaveSO.cs
+++ Assets/Scripts/SaveSystem/SO/PlayerStatusSaveSO.cs
@@ -5,13 +5,15 @@ public class PlayerStatusSaveSO : ScriptableObject
 {
     public PlayerStatusSave playerStatusSave;
 
-    public void NewGameReset( int saveSlot )
+    public void NewGameReset(int saveSlot)
     {
         playerStatusSave = new PlayerStatusSave();
 
         playerStatusSave.saveSlot = saveSlot;
         playerStatusSave.maxHealth = 6;
         playerStatusSave.currentHealth = playerStatusSave.maxHealth;
+        playerStatusSave.maxMagic = 30;
+        playerStatusSave.currentMagic = playerStatusSave.maxMagic;
         playerStatusSave.maxSouls = 1000;
         playerStatusSave.currentSouls = playerStatusSave.maxSouls;
     }
