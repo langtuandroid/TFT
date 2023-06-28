e73c0a0419d980f766d6590e123fc0a36ad45cf6
liyo86 <luarbaz@gmail.com>
2023-06-28 11:17:04 +0200

    Sistema de dialogo completo

--- Assets/Scripts/Interactables/InfoPost.cs
+++ Assets/Scripts/Interactables/InfoPost.cs
@@ -1,17 +1,20 @@
-// ************ @autor: Álvaro Repiso Romero *************
+// ************ @autor: ï¿½lvaro Repiso Romero *************
+
+using System;
 using UnityEngine;
 
 public class InfoPost : MonoBehaviour, IInteractable
 {
     [SerializeField] private GameObject _exclamationIcon;
     [SerializeField] private string _infoPostMessage;
+    private Action onStopInteracting;
 
     public void Interact( Vector2 lookDirection )
     {
-        if ( lookDirection.y > 0 )
-            Debug.Log( _infoPostMessage );
-        else
-            Debug.Log( "Cannot Read From Here" );
+        if (lookDirection.y > 0)
+        {
+            MyDialogueManager.Instance.Text(_infoPostMessage);
+        }
     }
 
     public void ShowCanInteract( bool show )
