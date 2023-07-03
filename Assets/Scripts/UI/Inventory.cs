5a0c755120ab7355f266dd9985dfefe3d13f8000
davih22997 <davih22997@gmail.com>
2023-07-03 23:26:31 +0200

    Ajustes en formato de textos y localización de botones.

--- Assets/Scripts/UI/Inventory.cs
+++ Assets/Scripts/UI/Inventory.cs
@@ -33,6 +33,9 @@ public class Inventory : MonoBehaviour
     private List<Button> _consumableButtons;
 
     [SerializeField]
+    [Tooltip("Parte en la que se ve el nombre del objeto seleccionado")]
+    private TMP_Text _titleInfo;
+    [SerializeField]
     [Tooltip("Parte que muestra la descripción del objeto seleccionado")]
     private TMP_Text _textInfo;
 
@@ -55,6 +58,7 @@ public class Inventory : MonoBehaviour
     //private GameInputs _gameInputs;
 
     // INFORMATION
+    private List<string> _titles;
     private List<string> _descriptions;
 
     // ELEMENTS
@@ -71,37 +75,58 @@ public class Inventory : MonoBehaviour
     {
         navigateAction = _inputActions.FindActionMap("UI").FindAction("Navigate");
 
+        _titles = new List<string>();
+
         _descriptions = new List<string>();
 
         // OBJETOS NO EQUIPABLES
-        _descriptions.Add("VARA MÁGICA\nLa vara que te regaló papá. Se puede usar para golpear e invocar poderes mágicos.");
-        _descriptions.Add("BOTAS DE SALTO\nUnas botas mágicas con las que podrás saltar muy alto.");
-        _descriptions.Add("ALETAS\nCon ellas podrás meterte en el agua y nadar sin miedo a ahogarte.");
-        _descriptions.Add("DASH\nTe permite realizar un ligero teletransporte.");
+        _titles.Add("VARA MÁGICA");
+        _descriptions.Add("La vara que te regaló papá. Se puede usar para golpear e invocar poderes mágicos.");
+        _titles.Add("BOTAS DE SALTO");
+        _descriptions.Add("Unas botas mágicas con las que podrás saltar muy alto.");
+        _titles.Add("ALETAS");
+        _descriptions.Add("Con ellas podrás meterte en el agua y nadar sin miedo a ahogarte.");
+        _titles.Add("DASH");
+        _descriptions.Add("Te permite realizar un ligero teletransporte.");
 
         // MAGIAS PRIMARIAS
         // Fuego
-        _descriptions.Add("MAGIA DE FUEGO\nUsa el poder del fuego para quemar a tus enemigos");
-        _descriptions.Add("BOLA DE FUEGO\nLanza una bola de fuego en línea recta");
-        _descriptions.Add("LANZALLAMAS\nUtiliza el poder del lanzallamas para quemar a los enemigos ante ti");
-        _descriptions.Add("RÁFAGA DE FUEGO\nInvoca una ráfaga de fuego que devasta todo a su alrededor");
+        _titles.Add("MAGIA DE FUEGO");
+        _descriptions.Add("Usa el poder del fuego para quemar a tus enemigos.");
+        _titles.Add("BOLA DE FUEGO");
+        _descriptions.Add("Lanza una bola de fuego en línea recta.");
+        _titles.Add("LANZALLAMAS");
+        _descriptions.Add("Utiliza el poder del lanzallamas para quemar a los enemigos ante ti.");
+        _titles.Add("RÁFAGA DE FUEGO");
+        _descriptions.Add("Invoca una ráfaga de fuego que devasta todo a su alrededor.");
         // Planta
-        _descriptions.Add("MAGIA DE PLANTA\n");
-        _descriptions.Add("MAGIA DÉBIL DE PLANTA\n");
-        _descriptions.Add("MAGIA MEDIA DE PLANTA\n");
-        _descriptions.Add("MAGIA FUERTE DE PLANTA\n");
+        _titles.Add("MAGIA DE PLANTA");
+        _descriptions.Add("Información sobre la magia de planta.");
+        _titles.Add("MAGIA DÉBIL DE PLANTA");
+        _descriptions.Add("Información sobre la magia débil de planta.");
+        _titles.Add("MAGIA MEDIA DE PLANTA");
+        _descriptions.Add("Información sobre la magia media de planta.");
+        _titles.Add("MAGIA FUERTE DE PLANTA");
+        _descriptions.Add("Información sobre la magia fuerte de planta.");
         // Agua
-        _descriptions.Add("MAGIA DE AGUA\n");
-        _descriptions.Add("MAGIA DÉBIL DE AGUA\n");
-        _descriptions.Add("MAGIA MEDIA DE AGUA\n");
-        _descriptions.Add("MAGIA FUERTE DE AGUA\n");
+        _titles.Add("MAGIA DE AGUA");
+        _descriptions.Add("Información sobre la magia de agua.");
+        _titles.Add("MAGIA DÉBIL DE AGUA");
+        _descriptions.Add("Información sobre la magia débil de agua.");
+        _titles.Add("MAGIA MEDIA DE AGUA");
+        _descriptions.Add("Información sobre la magia media de agua.");
+        _titles.Add("MAGIA FUERTE DE AGUA");
+        _descriptions.Add("Información sobre la magia fuerte de agua.");
 
         // MAGIAS SECUNDARIAS
-        _descriptions.Add("BOLA DE LUZ\nLanza una bola de luz que te permite ver a oscuras.");
-        _descriptions.Add("MAGIA DE AIRE\nTe permite lanzar ráfagas de aire para empujar a tus enemigos");
+        _titles.Add("BOLA DE LUZ");
+        _descriptions.Add("Lanza una bola de luz que te permite ver a oscuras.");
+        _titles.Add("MAGIA DE AIRE");
+        _descriptions.Add("Te permite lanzar ráfagas de aire para empujar a tus enemigos.");
 
         // OBJETOS CONSUMIBLES
-        _descriptions.Add("COGER OBJETOS PESADOS\nCon ello podrás alzar objetos muy pesados.");
+        _titles.Add("COGER OBJETOS PESADOS");
+        _descriptions.Add("Con ello podrás alzar objetos muy pesados.");
     }
 
     private void Update()
@@ -123,7 +148,7 @@ public class Inventory : MonoBehaviour
             button.onClick.AddListener(() => Debug.Log(button.gameObject.name));
 
         if (_playerStatusSO.playerStatusSave.isPhysicAttackUnlocked)
-            SetText(_descriptions[0]);
+            SetText(_titles[0], _descriptions[0]);
     }
 
     private void OnDestroy()
@@ -253,30 +278,30 @@ public class Inventory : MonoBehaviour
             // Objetos no equipables
             case 0: // Cetro
                 if (ps.isPhysicAttackUnlocked)
-                    SetText(_descriptions[0]);
+                    SetText(_titles[0], _descriptions[0]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 1: // Botas
                 if (ps.isJumpUnlocked)
-                    SetText(_descriptions[1]);
+                    SetText(_titles[1], _descriptions[1]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 2: // Nadar
                 if (ps.isSwimUnlocked)
-                    SetText(_descriptions[2]);
+                    SetText(_titles[2], _descriptions[2]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 3: // Dash
                 if (ps.isDashUnlocked)
-                    SetText(_descriptions[3]);
+                    SetText(_titles[3], _descriptions[3]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             default: // Elemento no definido
-                SetText("");
+                SetText("", "");
                 break;
         }
     }
@@ -288,78 +313,91 @@ public class Inventory : MonoBehaviour
         {
             case 0: // Magia de fuego
                 if (ps.isFireWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count]);
+                    SetText(_titles[_nonEquipableButtons.Count], 
+                        _descriptions[_nonEquipableButtons.Count]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 1: // Magia de fuego (débil)
                 if (ps.isFireWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 1]);
+                    SetText(_titles[_nonEquipableButtons.Count + 1], 
+                        _descriptions[_nonEquipableButtons.Count + 1]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 2: // Magia de fuego (media)
                 if (ps.isFireMediumUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 2]);
+                    SetText(_titles[_nonEquipableButtons.Count + 2], 
+                        _descriptions[_nonEquipableButtons.Count + 2]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 3: // Magia de fuego (fuerte)
                 if (ps.isFireStrongUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 3]);
+                    SetText(_titles[_nonEquipableButtons.Count + 3], 
+                        _descriptions[_nonEquipableButtons.Count + 3]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 4: // Magia de planta
                 if (ps.isPlantWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 4]);
+                    SetText(_titles[_nonEquipableButtons.Count + 4], 
+                        _descriptions[_nonEquipableButtons.Count + 4]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 5: // Magia de planta (débil)
                 if (ps.isPlantWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 5]);
+                    SetText(_titles[_nonEquipableButtons.Count + 5],
+                        _descriptions[_nonEquipableButtons.Count + 5]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 6: // Magia de planta (media)
                 if (ps.isPlantMediumUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 6]);
+                    SetText(_titles[_nonEquipableButtons.Count + 6],
+                        _descriptions[_nonEquipableButtons.Count + 6]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 7: // Magia de planta (fuerte)
                 if (ps.isPlantStrongUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 7]);
+                    SetText(_titles[_nonEquipableButtons.Count + 7],
+                        _descriptions[_nonEquipableButtons.Count + 7]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 8: // Magia de agua
                 if (ps.isWaterWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 8]);
+                    SetText(_titles[_nonEquipableButtons.Count + 8],
+                        _descriptions[_nonEquipableButtons.Count + 8]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 9: // Magia de agua (débil)
                 if (ps.isWaterWeakUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 9]);
+                    SetText(_titles[_nonEquipableButtons.Count + 9],
+                        _descriptions[_nonEquipableButtons.Count + 9]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 10: // Magia de agua (media)
                 if (ps.isWaterMediumUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 10]);
+                    SetText(_titles[_nonEquipableButtons.Count + 10],
+                        _descriptions[_nonEquipableButtons.Count + 10]);
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             case 11: // Magia de agua (fuerte)
                 if (ps.isWaterStrongUnlocked)
-                    SetText(_descriptions[_nonEquipableButtons.Count + 11]);
+                    SetText(_titles[_nonEquipableButtons.Count + 11],
+                        _descriptions[_nonEquipableButtons.Count + 11]
+                        );
                 else
-                    SetText("");
+                    SetText("", "");
                 break;
             default: // Elemento no definido
-                SetText("");
+                SetText("", "");
                 break;
         }
     }
@@ -372,7 +410,7 @@ public class Inventory : MonoBehaviour
                 break;
             case 1: // Aire
             default: // Elemento no definido
-                SetText("");
+                SetText("", "");
                 break;
         }
     }
@@ -384,7 +422,7 @@ public class Inventory : MonoBehaviour
             case 0: // Baya de vida
                 break;
             default: // Elemento no definido
-                SetText("");
+                SetText("", "");
                 break;
         }
     }
@@ -393,9 +431,10 @@ public class Inventory : MonoBehaviour
     /// Cambia el texto descriptivo según el texto enviado
     /// </summary>
     /// <param name="text"></param>
-    private void SetText(string text)
+    private void SetText(string name, string description)
     {
-        _textInfo.text = text;
+        _titleInfo.text = name;
+        _textInfo.text = description;
     }
 
     #endregion
