bbf235d00b9d30048d1231e7a04a60af69e4c1e0
davih22997 <davih22997@gmail.com>
2023-07-19 17:18:15 +0200

    Correcciones varias en pause de inventario.

--- Assets/Scripts/UI/Inventory/TemporalPauseInventory.cs
+++ Assets/Scripts/UI/Inventory/TemporalPauseInventory.cs
@@ -15,6 +15,10 @@ public class TemporalPauseInventory : MonoBehaviour
         gameInputs.OnPausePerformed += OnPausePerformed;
     }
 
+    private void OnDestroy()
+    {
+        gameInputs.OnPausePerformed -= OnPausePerformed;
+    }
 
     private void OnPausePerformed()
     {
