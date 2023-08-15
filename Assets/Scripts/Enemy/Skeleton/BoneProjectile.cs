67443a9a47740a6c7ce66ed599c721eb90f17e11
alvaroRepRom <104201349+alvaroRepRom@users.noreply.github.com>
2023-08-15 12:56:40 +0200

    Add centripetal force to bone

--- Assets/Scripts/Enemy/Skeleton/BoneProjectile.cs
+++ Assets/Scripts/Enemy/Skeleton/BoneProjectile.cs
@@ -2,11 +2,14 @@ using UnityEngine;
 
 public class BoneProjectile : MonoBehaviour
 {
+    [SerializeField] private float _speed = 6;
+
     private Rigidbody2D _rb;
-    [SerializeField]private float _speed = 6;
     private int   _damage;
-    private float _timer;
+    private float _deathTimer;
+    private Vector2 _initialDirection;
     private Vector2 _direction;
+    private Transform _target;
 
     private void Awake()
     {
@@ -15,22 +18,34 @@ public class BoneProjectile : MonoBehaviour
 
     private void Update()
     {
-        _timer -= Time.deltaTime;
-        if ( _timer < 0 )
+        _deathTimer -= Time.deltaTime;
+        if ( _deathTimer < 0 )
             gameObject.SetActive( false );
     }
 
     private void FixedUpdate()
     {
+        var distance = Vector2.Distance( transform.position, _target.position );
+        if ( distance < 3 )
+        {
+            Debug.Log( distance );
+            Vector2 centripetideDir = _target.position - transform.position;
+
+            _direction = 5 * _initialDirection + centripetideDir;
+            _direction.Normalize();
+        }
+
         _rb.MovePosition( _rb.position + Time.deltaTime * _speed * _direction );
     }
 
     public void Launch( Transform target , int damage )
     {
-        _direction = target.position - transform.position;
-        _direction.Normalize();
+        _initialDirection = target.position - transform.position;
+        _initialDirection.Normalize();
+        _direction = _initialDirection;
+        _target = target;
         _damage = damage;
-        _timer = 4;
+        _deathTimer = 4;
     }
 
     private void OnCollisionEnter2D( Collision2D collision )
